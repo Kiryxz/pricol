e60de4b7929d299f012656f27a8e4c2416e1187d
Kiryxz <127857903+Kiryxz@users.noreply.github.com>
2023-03-25 16:31:56 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+let MyText = "Съебался отсюда";
+alert("" + MyText);
+window.close();
+a = confirm("Закрыть вкладку?");
+if (a == true)
+{
+  window.close();
+}
+else
+{
+  alert("Ну и хуй с тобой");
+}
